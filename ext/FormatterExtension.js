cfbf43439a428439c8b1e722c4033f910dae22f4
Steven Cao <steven.cao@ernox.com>
2024-04-08 06:34:20 +0000

    final commit

--- ext/FormatterExtension.js
+++ ext/FormatterExtension.js
@@ -20,11 +20,50 @@ sap.ui.define([
                 return "None"
             }
         };
+
+        CardExtension.prototype.overallStatus = function (val) {
+            if (val === "C"){
+                return "Success";
+            } else {
+                return "Error";
+            }
+        }
+
+        CardExtension.prototype.getData = function (arg) {
+            console.log(arg);
+            var oCard = this.getCard();
+
+            
+            return oCard.request({
+                "url" : "{{destinations.ES5}}/sap/opu/odata/iwbep/GWSAMPLE_BASIC/SalesOrderSet?$top=10",
+                "withCredentials": true,
+                "parameters" : {
+                    "$orderby" : "CreatedAt desc",
+                    "$skip": arg,
+                    "$top" : "10"
+                }
+            }).then(function(oData){
+                let aDataBody = oData.d.results;
+                aDataBody.forEach((element, index, array) => {
+                    if (element.BillingStatus == "P" && element.DeliveryStatus == "D"){
+                        element.OverallStatus = "C";
+                        element.OverallStatusText = "Completed";
+                    } else {
+                        element.OverallStatus = "I";
+                        element.OverallStatusText = "Incomplete";
+                    }
+                });
+
+                return oData;
+            })
+        };
         
         CardExtension.prototype.getFormatters = function() {
             return {
                 deliveryStatus: this.deliveryStatus,
-                billingStatus: this.billingStatus
+                billingStatus: this.billingStatus,
+                overallStatus: this.overallStatus,
+                getData: this.getData
             }
         };
     
